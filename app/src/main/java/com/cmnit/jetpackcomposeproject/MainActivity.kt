0cf4442794c3f9d18115cfc64fc7f61ccd4e7334
NicoTic <1515828725@qq.com>
2021-04-30 09:57:29 +0800

    commit all files

---
+++ app/src/main/java/com/cmnit/jetpackcomposeproject/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.cmnit.jetpackcomposeproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name:String){
+    Text(text = "Hello $name")
+}
+
+@Preview
+@Composable
+fun PreviewGreeting(){
+    Greeting("Android")
+}
+
+
